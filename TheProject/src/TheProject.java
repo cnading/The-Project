b5b815721cac63d511de3278a036547bf8930656
cole.nading <cole.nading@MU-C1-10.mullen.pvt.k12.co.us>
2019-09-11 12:17:05 -0600

    JPanel

--- TheProject/src/TheProject.java
+++ TheProject/src/TheProject.java
@@ -1,4 +1,8 @@
+import java.awt.event.KeyEvent;
 import java.util.Scanner;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 public class TheProject
 	{
@@ -6,12 +10,16 @@ public class TheProject
 		static User hero;
 		static String newWordOne;
 		static String newWordTwo;
+		static JMenuBar menuBar;
+		static JMenu menu, submenu;
 
 		public static void main(String[] args)
 			{
-				greetUser();
-				getGender();
-				getInts();
+				// greetUser();
+				// getGender();
+				// getInts();
+				jPanel();
+
 			}
 
 		private static void greetUser()
@@ -34,9 +42,11 @@ public class TheProject
 						+ newWordTwo.substring(0, 1).toUpperCase()
 						+ newWordTwo.substring(1, newWordTwo.length()).toLowerCase());
 			}
+
 		private static void getGender()
 			{
-				System.out.println("Well " + hero.getName() + " , welcome to my diet writing program. With the answers to the following questions"
+				System.out.println("Well " + hero.getName()
+						+ " , welcome to my diet writing program. With the answers to the following questions"
 						+ "I will write a cutom meal plan for the entire day!");
 				System.out.println("So " + hero.getName() + " are you a male or a female?");
 				String gender = userInput.nextLine();
@@ -44,25 +54,33 @@ public class TheProject
 					{
 						System.out.println("Gotcha, thanks Mr. " + hero.getName());
 						hero.setGender(gender);
-					}
-				else if (gender.equals("Female") || gender.equals("female"))
+					} else if (gender.equals("Female") || gender.equals("female"))
 					{
 						System.out.println("Gotcha, thanks Ms. " + hero.getName());
-					}
-				else
+					} else
 					{
 						System.out.println("Sorry I don't understand, try again! (Male or Female)");
 						getGender();
 					}
 			}
+
 		public static void getInts()
 			{
 				System.out.println("Okay, now that we have your name and gender...");
 				System.out.println("How many inches tall are you?");
-				int height= userInput.nextInt();
+				int height = userInput.nextInt();
 				hero.setHeight(height);
 				System.out.println("");
 			}
-		}
 
-	
+		public static void jPanel()
+			{
+				menuBar = new JMenuBar();
+				menu = new JMenu("A Menu");
+				menu.setMnemonic(KeyEvent.VK_A);
+				menu.getAccessibleContext()
+						.setAccessibleDescription("The only menu in this program that has menu items");
+				menuBar.add(menu);
+
+			}
+	}
