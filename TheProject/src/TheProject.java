d49996f091f7251a0be56d74a817c12809a2b9af
cole.nading <cole.nading@MU-C1-10.mullen.pvt.k12.co.us>
2019-10-10 11:55:15 -0600

    Making actual plan

--- TheProject/src/TheProject.java
+++ TheProject/src/TheProject.java
@@ -39,19 +39,24 @@ public class TheProject extends JFrame
 		static int height;
 		static int results = 0;
 		static int gender;
+		static int minutes;
 		static int goAgain;
+		static int livingCalories;
 		static int calories= 0;
 		static int totalCalories;
+		static int caloriesBurned;
 		static String goal;
+		static String excercise;
 		static String adventureType;
 
 		static JFrame frame = new JFrame();
+
 
 		public static void main(String[] args)
 			{
 				hero = new User("", "", 0, 0, 0, 0);
 				getInfo();
-				calculations();
+				calorieCalculations();
 				displayMealPlan();
 			}
 
@@ -64,15 +69,27 @@ public class TheProject extends JFrame
 						"Well " + hero.getName()
 								+ " , welcome to my diet writing program. With the answers to the\n following questions"
 								+ " I will write a cutom meal plan for the entire day!");
+				
 				final String[] type =
 					{ "Gain Muscle and Fat", "Gain Muscle", "Keep Fit", "Lose Fat", "Lose Fat and Muscle" };
 				final JFrame frame = new JFrame();
 				goal = (String) JOptionPane.showInputDialog(frame, "What are your weight goals?", "Goal",
 						JOptionPane.QUESTION_MESSAGE, null, type, type[0]);
-
+				
+				 String [] type1 =
+							{ "Not at all", "Little", "Moderatley", "Hard", "Extreme" };
+						final JFrame frame1 = new JFrame();
+						excercise = (String) JOptionPane.showInputDialog(frame, "What are your plans for excercise today?", "Excercise",
+								JOptionPane.QUESTION_MESSAGE, null, type1, type1[0]);
+						
+						String minutesS = "";
+						minutesS = JOptionPane.showInputDialog("And how many total minutes do you plan to workout today?");
+						minutes = Integer.parseInt(minutesS);
+						
+						
 				Object[] options =
 					{ "Male", "Female" };
-				gender = JOptionPane.showOptionDialog(frame, "What is your sex?", "Your Sex",
+				gender = JOptionPane.showOptionDialog(frame1, "What is your sex?", "Your Sex",
 						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 				String weightS = "";
 				String heightS = "";
@@ -88,15 +105,15 @@ public class TheProject extends JFrame
 				hero.setWeight(weight);
 				Object[] choices =
 					{ "Yes", "No" };
-				goAgain = JOptionPane.showOptionDialog(frame,
+				goAgain = JOptionPane.showOptionDialog(frame1,
 						name + ", please confirm the following information is correct!\n" + "You weigh " + hero.getWeight()
-								+ " pounds\n" + "Are " + hero.getHeight() + " inches tall" + " and are " + hero.getAge() + " years young",
+								+ " killograms\n" + "Are " + hero.getHeight() + " centimeters tall" + " and are " + hero.getAge() + " years young",
 						"Last Chance", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, choices,
 						choices[0]);
 				if (goAgain == 0)
 					{
-						JOptionPane.showMessageDialog(frame, hero.getName() + ", generating your custom meal plan...");
-						calculations ();
+						JOptionPane.showMessageDialog(frame1, hero.getName() + ", calculating your calories...");
+						
 					} else
 					{
 						getInfo();
@@ -104,10 +121,40 @@ public class TheProject extends JFrame
 
 			}
 
-		public static void calculations()
+		public static void calorieCalculations()
 			{  //2845
-				if (gender==0){
-				totalCalories= (int) ((hero.getWeight() * 10) + ((6.25* hero.getHeight()) - (5* hero.getAge())) +5);
+				//metabolic equivalent
+				switch (excercise) {
+				case "Not at all": 
+					
+					
+				case "Little":
+					caloriesBurned = (int) (3*3.5 * hero.getWeight());
+					caloriesBurned= caloriesBurned / minutes;
+					totalCalories= totalCalories + caloriesBurned;
+					break;
+				case "Moderatley":
+					caloriesBurned = (int) (5*3.5 * hero.getWeight());
+					caloriesBurned= caloriesBurned / minutes;
+					totalCalories= totalCalories + caloriesBurned;
+					
+					break;
+				case "Hard":
+					caloriesBurned = (int) (7*3.5 * hero.getWeight());
+					caloriesBurned= caloriesBurned / minutes;
+					totalCalories= totalCalories + caloriesBurned;
+					
+					break;
+				case "Extreme":
+					caloriesBurned = (int) (10*3.5 * hero.getWeight());
+					caloriesBurned= caloriesBurned / minutes;
+					totalCalories= totalCalories + caloriesBurned;
+					break;
+				default:
+					
+					getInfo();
+				}
+				
 					if(goal.equals("Gain Muscle and Fat"))
 						{
 							totalCalories=totalCalories + 1000;
@@ -131,19 +178,31 @@ public class TheProject extends JFrame
 						{
 							getInfo();
 						}
+					if (gender==0){
+						livingCalories= (int) ((hero.getWeight() * 10) + ((6.25* hero.getHeight()) - (5* hero.getAge())) +5);
+						totalCalories = totalCalories + livingCalories;
+					}
+						
+					else if (gender==1){
+						livingCalories= (int) ((hero.getWeight() * 10) + ((6.25* hero.getHeight()) - (5* hero.getAge())) -161);
+						totalCalories = totalCalories + livingCalories;
+							}
+					else
+						{
+							getInfo();
+						}
 				JOptionPane.showMessageDialog(frame,
 						"Well " + hero.getName() + ", You need to eat " + totalCalories + " calories to maintain your weight");
 				}
-				else if (gender==1){
-					totalCalories= (int) (hero.getWeight() * 10 + 6.25* hero.getHeight() - 5* hero.getAge()-161);
-					JOptionPane.showMessageDialog(frame,
-							"Well " + hero.getName() + ", You need to eat " + totalCalories + " calories to to maintain your weight");
-					}
 				
-			}
+				
+			
 
 		public static void displayMealPlan()
 			{
-
+				if (totalCalories > 2500){
+					
+				}
+				
 			}
 	}
